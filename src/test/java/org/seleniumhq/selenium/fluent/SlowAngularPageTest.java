071dc1899e97c432970b261d7dc36a5065caa585
Paul Hammant <paul@hammant.org>
2013-08-15 22:14:06 -0500

    slow angular page functionality

--- src/test/java/org/seleniumhq/selenium/fluent/SlowAngularPageTest.java
+++ src/test/java/org/seleniumhq/selenium/fluent/SlowAngularPageTest.java
@@ -4,13 +4,13 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class StaleRecoveringTest {
+public class SlowAngularPageTest {
 
     @Test
-    public void knownStaleCaseCanBeOvercome() {
+    public void slowAngularPageTriesToPushFluentSeleniumsButtons() {
         FirefoxDriver driver = new FirefoxDriver();
 
-        driver.get("http://seleniumhq.github.io/fluent-selenium/2915/index.html");
+        driver.get("http://seleniumhq.github.io/fluent-selenium/vvvvSlowRendering.html");
 
         FluentWebDriver fwd = new FluentWebDriver(driver);
 
