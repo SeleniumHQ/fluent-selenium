f12a66b0fdaabb4c95ee2020e88d3d7660a8f41f
Paul Hammant <paul@hammant.org>
2014-08-14 13:51:15 -0400

    work on stale element handling, and dep upgrades

--- java/src/test/java/org/seleniumhq/selenium/fluent/StaleRecoveringTest.java
+++ java/src/test/java/org/seleniumhq/selenium/fluent/StaleRecoveringTest.java
@@ -2,12 +2,42 @@ package org.seleniumhq.selenium.fluent;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static com.thoughtworks.selenium.SeleneseTestBase.assertEquals;
+import static junit.framework.TestCase.fail;
 
 public class StaleRecoveringTest {
 
+    @Test(timeout = 60000)
+    public void vanillaWebDriverShouldCatchStaleExceptionAndBeAbleToCarryOnAnyway() {
+
+        FirefoxDriver driver = new FirefoxDriver();
+
+        driver.get("http://seleniumhq.github.io/fluent-selenium/2915/index.html");
+        WebElement elem;
+        try {
+            elem = driver.findElement(By.id("cheddarCheeseLoginPassword"));
+            elem.sendKeys("bar");
+            getAndVerify(elem);
+            fail("should have barfed");
+        } catch(StaleElementReferenceException sere) {
+            elem = driver.findElement(By.id("cheddarCheeseLoginPassword"));
+            getAndVerify(elem);
+        } finally {
+            driver.quit();
+        }
+    }
+
+    private void getAndVerify(WebElement elem) {
+        String val = elem.getAttribute("value");
+        assertEquals("bar", val);
+    }
+
     @Test
-    public void knownStaleCaseCanBeOvercome() {
+    public void knownStaleCaseCanBeOvercomeInFluentWebDriverToo() {
         FirefoxDriver driver = new FirefoxDriver();
 
         driver.get("http://seleniumhq.github.io/fluent-selenium/2915/index.html");
@@ -21,4 +51,5 @@ public class StaleRecoveringTest {
             driver.quit();
         }
     }
+
 }
