8fc0ff1bb54db7cd5ecec91ad7e2638729339ac4
Paul Hammant <paul@hammant.org>
2014-08-14 19:34:39 -0400

    simulation of stale element exception

--- java/src/test/java/org/seleniumhq/selenium/fluent/StaleRecoveringTest.java
+++ java/src/test/java/org/seleniumhq/selenium/fluent/StaleRecoveringTest.java
@@ -2,9 +2,13 @@ package org.seleniumhq.selenium.fluent;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
 
 import static com.thoughtworks.selenium.SeleneseTestBase.assertEquals;
 import static junit.framework.TestCase.fail;
@@ -14,9 +18,11 @@ public class StaleRecoveringTest {
     @Test(timeout = 60000)
     public void vanillaWebDriverShouldCatchStaleExceptionAndBeAbleToCarryOnAnyway() {
 
-        FirefoxDriver driver = new FirefoxDriver();
+        // We have to simulate a stale element exception as
+        // we can't find a real-world example
+        FirstGetAttributeIsStaleDriver driver = new FirstGetAttributeIsStaleDriver();
 
-        driver.get("http://seleniumhq.github.io/fluent-selenium/2915/index.html");
+        driver.get("http://seleniumhq.github.io/fluent-selenium/7753/index.html");
         WebElement elem;
         try {
             elem = driver.findElement(By.id("cheddarCheeseLoginPassword"));
@@ -29,6 +35,9 @@ public class StaleRecoveringTest {
         } finally {
             driver.quit();
         }
+
+        assertEquals(2, driver.countOfGetAttribute[0]);
+
     }
 
     private void getAndVerify(WebElement elem) {
@@ -38,9 +47,12 @@ public class StaleRecoveringTest {
 
     @Test
     public void knownStaleCaseCanBeOvercomeInFluentWebDriverToo() {
-        FirefoxDriver driver = new FirefoxDriver();
 
-        driver.get("http://seleniumhq.github.io/fluent-selenium/2915/index.html");
+        // We have to simulate a stale element exception as
+        // we can't find a real-world example
+        FirstGetAttributeIsStaleDriver driver = new FirstGetAttributeIsStaleDriver();
+
+        driver.get("http://seleniumhq.github.io/fluent-selenium/7753/index.html");
 
         FluentWebDriver fwd = new FluentWebDriver(driver);
 
@@ -50,6 +62,83 @@ public class StaleRecoveringTest {
         } finally {
             driver.quit();
         }
+
+        assertEquals(2, driver.countOfGetAttribute[0]);
+
     }
 
+    private static class FirstGetAttributeIsStaleDriver extends FirefoxDriver {
+        public final int[] countOfGetAttribute = {0};
+
+        @Override
+        public WebElement findElementById(String using) {
+            final WebElement we = super.findElementById(using);
+            return new WebElement() {
+                public void click() {
+                    we.click();
+                }
+
+                public void submit() {
+                    we.submit();
+                }
+
+                public void sendKeys(CharSequence... keysToSend) {
+                    we.sendKeys(keysToSend);
+                }
+
+                public void clear() {
+                    we.clear();
+                }
+
+                public String getTagName() {
+                    return we.getTagName();
+                }
+
+                public String getAttribute(String name) {
+                    if (countOfGetAttribute[0] == 0) {
+                        countOfGetAttribute[0]++;
+                        throw new StaleElementReferenceException("boop");
+                    }
+                    countOfGetAttribute[0]++;
+                    return we.getAttribute(name);
+                }
+
+                public boolean isSelected() {
+                    return we.isSelected();
+                }
+
+                public boolean isEnabled() {
+                    return we.isEnabled();
+                }
+
+                public String getText() {
+                    return we.getText();
+                }
+
+                public List<WebElement> findElements(By by) {
+                    return we.findElements(by);
+                }
+
+                public WebElement findElement(By by) {
+                    return we.findElement(by);
+                }
+
+                public boolean isDisplayed() {
+                    return we.isDisplayed();
+                }
+
+                public Point getLocation() {
+                    return we.getLocation();
+                }
+
+                public Dimension getSize() {
+                    return we.getSize();
+                }
+
+                public String getCssValue(String propertyName) {
+                    return we.getCssValue(propertyName);
+                }
+            };
+        }
+    }
 }
